780d71191b7587fa9b5421cd29f11340eb77d336
bosilow <bosiljkostojanovic@gmail.com>
2022-01-10 20:11:58 +0100

    added facebook, linkedin, github links and resume.pdf file at the footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,28 +1,40 @@
 import "./footer.styles.scss";
 
+import resume from "../../assets/resume/resume-bosiljko-stojanovic.pdf";
+
 const Footer = ({ text }) => (
   <footer className="footer">
     <div className="social-icons">
-      <a
-        href="https://www.facebook.com/bosiljko.stojanovic"
-        class="fa fa-facebook"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-      </a>
-      <a
-        href="https://www.linkedin.com/in/bosiljko-stojanovic-0295241b6/"
-        class="fa fa-linkedin"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-      </a>
-      <a href="#" class="fa fa-file-pdf-o">
-        {" "}
-      </a>
+      <div>
+        <a
+          href="https://www.facebook.com/bosiljko.stojanovic"
+          class="fa fa-facebook"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+        </a>
+        <a
+          href="https://www.linkedin.com/in/bosiljko-stojanovic-0295241b6/"
+          class="fa fa-linkedin"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+        </a>
+        <a
+          href="https://github.com/bosilow"
+          class="fa fa-github"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+        </a>
+      </div>
     </div>
+    <a href={resume} className="resume-link" download>
+      Download resume
+    </a>
     <span className="copyright-text">{text}</span>
   </footer>
 );
